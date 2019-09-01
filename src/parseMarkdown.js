437d3fe85348e18c288c3f4bd27b5a3134ce0ae4
David Garner <dgarner@mintel.com>
2019-09-01 01:17:00 +0100

    Generalise parseMarkdown

--- src/parseMarkdown.js
+++ src/parseMarkdown.js
@@ -1,60 +1,109 @@
 const marked = require('marked');
-const moment = require('moment');
 
-function getHeadings(mdString) {
-  const lexerData = marked.lexer(mdString);
-  const headings = [];
-  let quadrant;
-  let currentHeading;
-  for (let token of lexerData) {
-    if (token.type === 'heading' && token.depth === 1) {
-      quadrant = token.text;
-    } else if (token.type === 'heading' && token.depth === 2) {
-      currentHeading = {
-        name: token.text,
-        values: [],
-      };
-      headings.push(currentHeading);
-    } else if (currentHeading) {
-      currentHeading.values.push(token);
+function splitByHeadings(tokens, depth) {
+  const sections = [];
+  let preamble = [];
+  let section;
+  for (let token of tokens) {
+    if (token.type === 'heading' && token.depth === depth) {
+      section = [];
+      sections.push(section);
+    }
+    if (section) {
+      section.push(token);
+    } else {
+      preamble.push(token);
     }
   }
-  return { quadrant, headings, links: lexerData.links };
+  return sections;
 }
 
-function partitionSection(tokens, links) {
-  const frontMatter = [];
+function toTree(tokens) {
+  if (!tokens.length || tokens[0].type !== 'heading') {
+    const start = tokens.findIndex(token => token.type === 'heading');
+    if (start === -1) {
+      return {
+        tokens: [],
+      };
+    }
+    return toTree(tokens.slice(start));
+  }
+
+  const firstSubheading =
+    tokens
+      .slice(1)
+      .findIndex(
+        token => token.type === 'heading' && token.depth > tokens[0].depth
+      ) + 1;
+
+  if (firstSubheading === 0) {
+    let endOfSection =
+      tokens.slice(1).findIndex(token => token.type === 'heading') + 1;
+    if (endOfSection === 0) {
+      endOfSection = tokens.length;
+    }
+    return {
+      name: tokens[0].text,
+      tokens: tokens.slice(1, endOfSection),
+    };
+  }
+
+  const depth = tokens[firstSubheading].depth;
+  let end = tokens
+    .slice(firstSubheading)
+    .findIndex(token => token.type === 'heading' && token.depth < depth);
+  if (end === -1) {
+    end = tokens.length;
+  } else {
+    end += firstSubheading;
+  }
+  const acc = {
+    name: tokens[0].text,
+    tokens: tokens.slice(1, firstSubheading),
+    sections: splitByHeadings(tokens.slice(firstSubheading, end), depth).map(
+      toTree
+    ),
+  };
+
+  return acc;
+}
+
+function partitionTokens(tokens, links) {
+  const metadata = {};
   const listStart = tokens.findIndex(token => token.type === 'list_start');
   const listEnd = tokens.findIndex(token => token.type === 'list_end');
   for (let i = listStart; i < listEnd; i++) {
     if (tokens[i].type === 'text') {
-      frontMatter.push(tokens[i].text);
+      let [key, value] = tokens[i].text.split(':').map(t => t.trim());
+      if (value) {
+        metadata[key.toLowerCase()] = value;
+      }
     }
   }
-  description = tokens.slice(listEnd + 1);
+  const markedTokens = tokens.slice(listEnd + 1);
+  let renderedMd = '';
+  if (markedTokens.length) {
+    markedTokens.links = links;
+    renderedMd = marked.parser(markedTokens).trim();
+  }
 
-  return { frontMatter, description, links };
+  return { metadata, renderedMd };
+}
+
+function mapNode({ name, tokens, sections }, links) {
+  const { metadata, renderedMd } = partitionTokens(tokens, links);
+  if (sections) {
+    content = sections.map(section => mapNode(section, links));
+  } else {
+    content = renderedMd;
+  }
+  return { name, metadata, content };
 }
 
 function parseMarkdown(mdString) {
-  const { quadrant, headings, links } = getHeadings(mdString);
-  return headings.map(({ name, values }) => {
-    const { frontMatter, description } = partitionSection(values);
-    const markedDescription = [...description];
-    markedDescription.links = links;
-    const blip = {
-      name,
-      description: marked.parser(markedDescription).trim(),
-    };
-    for (let token of frontMatter) {
-      let [key, value] = token.split(':').map(t => t.trim());
-      if (value) {
-        blip[key.toLowerCase()] = value;
-      }
-    }
-    blip.quadrant = quadrant;
-    return blip;
-  });
+  const tokens = marked.lexer(mdString);
+  const tree = toTree(tokens);
+  return mapNode(tree, tokens.links);
 }
 
 exports.getHeadings = getHeadings;
--- src/parseMarkdown.test.js
+++ src/parseMarkdown.test.js
@@ -1,118 +1,165 @@
 const {
-  getHeadings,
-  partitionSection,
   parseMarkdown,
+  splitByHeadings,
+  toTree,
 } = require('./parseMarkdown');
+const marked = require('marked');
 
-describe('getHeadings', () => {
-  it('finds and splits markdown by h2 headings', () => {
+describe('splitByHeadings', () => {
+  it('splits h1s', () => {
+    const md = `
+# First heading
+
+Content under first heading
+
+## X
+
+yyy
+
+### Z
+hhh
+
+# Second heading
+
+Content under second heading
+
+# Third heading
+
+Content under third heading
+    `.trim();
+    const tokens = marked.lexer(md);
+
+    expect(splitByHeadings(tokens, 1)).toEqual([
+      [
+        { type: 'heading', depth: 1, text: 'First heading' },
+        { type: 'paragraph', text: 'Content under first heading' },
+        { type: 'space' },
+        { type: 'heading', depth: 2, text: 'X' },
+        { type: 'paragraph', text: 'yyy' },
+        { type: 'space' },
+        { type: 'heading', depth: 3, text: 'Z' },
+        { type: 'paragraph', text: 'hhh' },
+        { type: 'space' },
+      ],
+      [
+        { type: 'heading', depth: 1, text: 'Second heading' },
+        { type: 'paragraph', text: 'Content under second heading' },
+        { type: 'space' },
+      ],
+      [
+        { type: 'heading', depth: 1, text: 'Third heading' },
+        { type: 'paragraph', text: 'Content under third heading' },
+      ],
+    ]);
+  });
+
+  it('splits by heading', () => {
     const md = `
 # H1
 
 ## H2
 
-Stuff
+- Item: One
+- Item Two without a colon
+
+Stuff with a [link][link]
+
+[link]: http://link
 
 ## Another h2
 
-- More
-- stuff
-    `.trim();
-    const { headings, links } = getHeadings(md);
-    expect(headings).toEqual([
-      {
-        name: 'H2',
-        values: [{ type: 'paragraph', text: 'Stuff' }, { type: 'space' }],
-      },
-      {
-        name: 'Another h2',
-        values: [
-          {
-            type: 'list_start',
-            ordered: false,
-            start: '',
-            loose: false,
-          },
-          {
-            type: 'list_item_start',
-            task: false,
-            loose: false,
-          },
-          { type: 'text', text: 'More' },
-          { type: 'list_item_end' },
-          {
-            type: 'list_item_start',
-            task: false,
-            loose: false,
-          },
-          { type: 'text', text: 'stuff' },
-          { type: 'list_item_end' },
-          { type: 'list_end' },
-        ],
-      },
-    ]);
-    expect(links).toEqual({});
-  });
+- Status: Cool
+- Really: Yep
 
-  it('returns an empty array when no h2 headings', () => {
-    const md = '# No h2s here';
-    expect(getHeadings(md)).toEqual({
-      headings: [],
-      quadrant: 'No h2s here',
-      links: {},
+Eh
+    `.trim();
+    const tokens = marked.lexer(md);
+    expect(toTree(tokens)).toEqual({
+      name: 'H1',
+      tokens: [],
+      sections: [
+        {
+          name: 'H2',
+          tokens: [
+            { type: 'list_start', ordered: false, start: '', loose: false },
+            {
+              type: 'list_item_start',
+              task: false,
+              checked: undefined,
+              loose: false,
+            },
+            { type: 'text', text: 'Item: One' },
+            { type: 'list_item_end' },
+            {
+              type: 'list_item_start',
+              task: false,
+              checked: undefined,
+              loose: false,
+            },
+            { type: 'text', text: 'Item Two without a colon' },
+            { type: 'space' },
+            { type: 'list_item_end' },
+            { type: 'list_end' },
+            { type: 'paragraph', text: 'Stuff with a [link][link]' },
+            { type: 'space' },
+          ],
+        },
+        {
+          name: 'Another h2',
+          tokens: [
+            { type: 'list_start', ordered: false, start: '', loose: false },
+            {
+              type: 'list_item_start',
+              task: false,
+              checked: undefined,
+              loose: false,
+            },
+            { type: 'text', text: 'Status: Cool' },
+            { type: 'list_item_end' },
+            {
+              type: 'list_item_start',
+              task: false,
+              checked: undefined,
+              loose: false,
+            },
+            { type: 'text', text: 'Really: Yep' },
+            { type: 'space' },
+            { type: 'list_item_end' },
+            { type: 'list_end' },
+            { type: 'paragraph', text: 'Eh' },
+          ],
+        },
+      ],
     });
   });
-});
 
-describe('partitionSection', () => {
-  it('partitions out the initial list items and the remaining markdown', () => {
+  it('ignores pre-h1 and repeated top level headers', () => {
     const md = `
-## Stuff
+This should be ignored.
 
-This line should be ignored.
+# H1
 
-- More
-- stuff
+Content
 
-Everything after this is kept...
+# Another h1
 
-- just
-  - pure
-  - markdown
+This should be ignored too.
     `.trim();
-    const tokens = getHeadings(md).headings[0].values;
-    const { frontMatter, description, links } = partitionSection(tokens, {});
-    expect(frontMatter).toEqual(['More', 'stuff']);
-    expect(description).toEqual([
-      { type: 'paragraph', text: 'Everything after this is kept...' },
-      { type: 'space' },
-      { type: 'list_start', ordered: false, start: '', loose: false },
-      {
-        type: 'list_item_start',
-        task: false,
-        loose: false,
-      },
-      { type: 'text', text: 'just' },
-      { type: 'list_start', ordered: false, start: '', loose: false },
-      {
-        type: 'list_item_start',
-        task: false,
-        loose: false,
-      },
-      { type: 'text', text: 'pure' },
-      { type: 'list_item_end' },
-      {
-        type: 'list_item_start',
-        loose: false,
-        task: false,
-      },
-      { type: 'text', text: 'markdown' },
-      { type: 'list_item_end' },
-      { type: 'list_end' },
-      { type: 'list_item_end' },
-      { type: 'list_end' },
-    ]);
-    expect(links).toEqual({});
+    const tokens = marked.lexer(md);
+    expect(toTree(tokens)).toEqual({
+      name: 'H1',
+      tokens: [{ type: 'paragraph', text: 'Content' }, { type: 'space' }],
+    });
+  });
+
+  it('returns an empty object when there are no headers', () => {
+    const md = `
+This should be ignored.
+    `.trim();
+    const tokens = marked.lexer(md);
+    expect(toTree(tokens)).toEqual({
+      tokens: [],
+    });
   });
 });
 
@@ -137,20 +184,81 @@ Stuff with a [link][link]
 
 Eh
   `.trim();
-    expect(parseMarkdown(md)).toEqual([
-      {
-        name: 'H2',
-        item: 'One',
-        description: '<p>Stuff with a <a href="http://link">link</a></p>',
-        quadrant: 'H1',
-      },
-      {
-        name: 'Another h2',
-        status: 'Cool',
-        really: 'Yep',
-        description: '<p>Eh</p>',
-        quadrant: 'H1',
-      },
-    ]);
+    expect(parseMarkdown(md)).toEqual({
+      name: 'H1',
+      metadata: {},
+      content: [
+        {
+          name: 'H2',
+          metadata: { item: 'One' },
+          content: '<p>Stuff with a <a href="http://link">link</a></p>',
+        },
+        {
+          name: 'Another h2',
+          metadata: { status: 'Cool', really: 'Yep' },
+          content: '<p>Eh</p>',
+        },
+      ],
+    });
+  });
+
+  it('ignores text which does not fit the format', () => {
+    const md = `
+## Stuff
+
+This line should be ignored.
+
+- More
+- stuff
+
+Everything after this is kept...
+
+- just
+  - pure
+  - markdown
+    `.trim();
+    expect(parseMarkdown(md)).toEqual({
+      name: 'Stuff',
+      metadata: {},
+      content:
+        '<p>Everything after this is kept...</p>\n<ul>\n<li>just<ul>\n<li>pure</li>\n<li>markdown</li>\n</ul>\n</li>\n</ul>',
+    });
+  });
+
+  it('returns an empty object when no h2 headings', () => {
+    const md = '# No h2s here';
+    expect(parseMarkdown(md)).toEqual({
+      name: 'No h2s here',
+      metadata: {},
+      content: '',
+    });
+  });
+
+  it.only('handles arbitrary levels', () => {
+    const md = `
+# H1
+
+### H3
+
+- Item: One
+
+## H2
+
+- Status: Cool
+- Really: Yep
+
+Eh
+  `.trim();
+    expect(parseMarkdown(md)).toEqual({
+      name: 'H1',
+      metadata: {},
+      content: [
+        {
+          name: 'H3',
+          metadata: { item: 'One' },
+          content: '',
+        },
+      ],
+    });
   });
 });
